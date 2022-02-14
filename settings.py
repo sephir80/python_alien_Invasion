7994a7012dfd39030d3b434f2b1ab1ecb88bc63f
Alessandro Tognon (mac) <sephir80@gmail.com>
2022-02-14 21:59:01 +0100

    create setting screen class

---
+++ settings.py
@@ -0,0 +1,12 @@
+import colorsys
+
+
+class Settings:
+    """A class to store all settings for alien invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
