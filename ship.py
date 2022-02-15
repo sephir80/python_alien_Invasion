574be192e68a68e862f422c3a9234ebdf354f90d
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-15 12:30:56 +0100

    create a float value to contain ship speed increment the cast to rect.x

--- ship.py
+++ ship.py
@@ -7,22 +7,35 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting poisition."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+
         # Load the ship image and get its rect. ----------------------------
         self.image = pygame.image.load('image/ship.png')
         self.rect = self.image.get_rect()
+
         # Start each new ship at the bottom center of the screen. ----------
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.---------
+        self.x = float(self.rect.x)
+
         # Movement flag-----------------------------------------------------
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag"""
+        #update the ship's x value not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed    # self.x  value is float
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x                    # self.rect.x can be only int
+
+
 
     def blitme(self):
         """Draw the ship at irs current location."""
