f443c32b8a31fa59e93ffbef5b7776b0314f6eb0
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-15 17:32:14 +0100

    update to missing mod( limit bullets, refactor updBullets)

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,8 @@ class AlienInvasion:
             # Controlla eventi della tastiera ed del mouse
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+#           print(len(self.bullets))    print number of bullets on screen
             self._update_screen()
 
     def _check_events(self):
@@ -55,8 +56,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         """respond to key-presses"""
@@ -64,6 +66,16 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _update_bullets(self):
+        """"update position of bullets and get rid of old bullets"""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
